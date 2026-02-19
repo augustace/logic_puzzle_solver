4c8a410ac9c0006c8ddcb60b326ec0047d0af4f9
Jongwoo Shin <jongmachine@Jongwoos-MacBook-Air.local>
2026-02-19 16:26:27 +0900

    Addition of model, recognition, extraction imported

--- README.md
+++ README.md
@@ -3,10 +3,21 @@ Automatic logic puzzle solver from image file to solution
 
 The project aims to create a mobile application which feeds in an image file and outputs a solution to the given logic puzzle.
 
-1. Use Tensorflow in R language to scan the image and return the type of logic puzzle as well as the four corners of the quadraliteral. (logic_puzzle_solver/cnn)
+0. Stack of puzzle images are stored in the folder for the sake of image training purpose (logic_puzzle_solver/cnn_python/photos)
+
+1. Use Pytorch to scan the image and return the type of logic puzzle as well as the four corners of the quadraliteral. (logic_puzzle_solver/cnn_python)
 
 2. For each type of a puzzle recognized, extract the raw information of the board required to solve the puzzle into a specific data structure (logic_puzzle_solver/extract)
 
 3. Given the puzzle type and a data structure including raw data, develop an efficient algorithm to return the answer. (logic_puzzle_solver/solver)
 
+
+
+Progress (plans)
+
+1. First goal is to recognize and differentiate sudoku puzzles and nonogram puzzles using general CNN mechanism available. (model.py and recognition.py)
+
+2. Each individual extraction from image to puzzle will happen on the individual extract.py files, which I will figure out if it's better to create one giant model that extracts metadata at once or multiple small models suited for each puzzles due to their unique shapes.
+
+3. Puzzle solving algorithms will be imported for the puzzle types which has algorithms fully developed and efficient. Otherwise, I will construct an efficient data structure and algorithm to its solution.
 
--- cnn_python/model.py
+++ cnn_python/model.py
@@ -1,21 +1,35 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import PIL
-import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5, padding='same')
+        torch.nn.init.kaiming_normal_(self.conv1.weight)
+        self.conv2 = nn.Conv2d(32, 32, 5, padding='same')
+        torch.nn.init.kaiming_normal_(self.conv2.weight)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding='same')
+        torch.nn.init.kaiming_normal_(self.conv3.weight)
+        self.conv4 = nn.Conv2d(64, 64, 3, padding='same')
+        torch.nn.init.kaiming_normal_(self.conv4.weight)
+        self.fc1 = nn.Linear(3136, 256)
+        torch.nn.init.kaiming_normal_(self.fc1.weight)
+        self.fc2 = nn.Linear(256, 10)
+        torch.nn.init.kaiming_normal_(self.fc2.weight)
+        self.dropout = nn.Dropout(0.25)
+        self.pool = nn.MaxPool2d(2, 2)
 
-import pathlib
-"""dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
-print(data_dir) """
-
-data_dir = pathlib.Path("/Users/jongmachine/.keras/datasets/flower_photos")
-
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
-roses = list(data_dir.glob('roses/*'))
-img = PIL.Image.open(str(roses[0]))
-img.show()
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout(x)
+        x = F.relu(self.conv3(x))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.dropout(x)
+        x = torch.flatten(x, 1) 
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+        return x
